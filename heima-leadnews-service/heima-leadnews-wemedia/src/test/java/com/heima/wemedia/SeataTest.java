8875bef1127d1041ace3a62c055d33a2b4f1022c
finn <yzhe29028@gmail.com>
2025-10-21 18:17:43 +0800

    完成day04的全部内容包含(seata)

--- heima-leadnews-service/heima-leadnews-wemedia/src/test/java/com/heima/wemedia/SeataTest.java
+++ heima-leadnews-service/heima-leadnews-wemedia/src/test/java/com/heima/wemedia/SeataTest.java
@@ -1,13 +1,7 @@
 package com.heima.wemedia;
 
 import com.heima.apis.article.ArticleOpenFeignClient;
-import com.heima.model.wemedia.pojos.WmNews;
 import com.heima.wemedia.mapper.WmNewsMapper;
-import com.heima.wemedia.service.WmNewsService;
-import com.heima.wemedia.service.impl.TestService;
-import com.heima.wemedia.service.impl.WmNewsServiceImpl;
-import io.seata.core.context.RootContext;
-import io.seata.spring.annotation.GlobalTransactional;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,11 +15,11 @@ public class SeataTest {
     private WmNewsMapper wmNewsMapper;
     @Resource
     private ArticleOpenFeignClient articleOpenFeignClient;
-    @Resource
-    private TestService testService;
+//    @Resource
+//    private TestService testService;
     //seata测试
-    @Test
-    public void test(){
-            testService.doTest();
-    }
+//    @Test
+//    public void test(){
+////            testService.doTest();
+//    }
 }
