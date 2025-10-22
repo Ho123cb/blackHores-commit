b19173d65095c4d394580dcf3b63e620efbc2114
finn <yzhe29028@gmail.com>
2025-10-22 16:10:26 +0800

    初步完成延迟任务架子的完成，准备填充其逻辑代码部分

---
+++ heima-leadnews-service/heima-leadnews-schedule/src/main/java/com/heima/schedule/ScheduleApplication.java
@@ -0,0 +1,19 @@
+package com.heima.schedule;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+@MapperScan("com.heima.schedule.mapper")
+public class ScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleApplication.class,args);
+    }
+}
