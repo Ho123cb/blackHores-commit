250ca7d5c29d11ec0678f63e40e3542b6ed47957
finn <yzhe29028@gmail.com>
2025-10-27 15:48:59 +0800

    完成day08管理员频道相关所有功能(补加频道名称重复判断)

--- heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/impl/WmChannelServiceImpl.java
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/impl/WmChannelServiceImpl.java
@@ -56,6 +56,13 @@ public class WmChannelServiceImpl extends ServiceImpl<WmChannelMapper, WmChannel
         if(adChannel == null || StringUtils.isBlank(adChannel.getName()))
             return ResponseResult.errorResult(AppHttpCodeEnum.PARAM_INVALID);
         WmChannel wmChannel = new WmChannel();
+
+        //判断频道名称是否重复
+        LambdaQueryWrapper<WmChannel> lq = new LambdaQueryWrapper<>();
+        lq.eq(WmChannel::getName,adChannel.getName());
+        WmChannel wmChannel1 = wmChannelMapper.selectOne(lq);
+        if(wmChannel1 != null)
+            return ResponseResult.errorResult(AppHttpCodeEnum.DATA_EXIST);
         try {
             BeanUtils.copyProperties(wmChannel, adChannel);
         } catch (Exception e) {
